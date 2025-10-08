dcfc253ad28782e533d6373fa5507ab5e57806e5
sujay5372bot <sujay53721@gmail.com>
2025-10-08 16:13:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", "24935727"))
+API_HASH = os.getenv("API_HASH", "3fd33336629324ecd664e9b6894f0909")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7623238714:AAFC7Eq7yUkcIIkupp3ZWDnFwdPgg5i4FH4")
 
 # Facebook Page Config
 FB_PAGE_ID = os.getenv("FB_PAGE_ID", "")

12f6c1d5c7c1637268d4d41dff99d5066d7892d9
sujay5372bot <sujay53721@gmail.com>
2025-10-08 16:12:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # Facebook Page Config
-FB_PAGE_ID = os.getenv("FB_PAGE_ID")
-FB_ACCESS_TOKEN = os.getenv("FB_ACCESS_TOKEN")
+FB_PAGE_ID = os.getenv("FB_PAGE_ID", "")
+FB_ACCESS_TOKEN = os.getenv("FB_ACCESS_TOKEN", "")
 
 # Optional: Admin and Premium Control
-ADMINS = [123456789]  # Telegram User IDs
+ADMINS = [6942557751]  # Telegram User IDs
